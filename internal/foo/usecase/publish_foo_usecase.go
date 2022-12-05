22f8843bf6609517303b6a3fd027b366374d873d
Techmista <camilotxm@gmail.com>
2022-12-05 16:56:03 -0300

    modified:   internal/foo/usecase/publish_foo_usecase.go

--- internal/foo/usecase/publish_foo_usecase.go
+++ internal/foo/usecase/publish_foo_usecase.go
@@ -1,1 +1,9 @@
 package usecase
+
+import (
+	"github.com/qbem-repository/golang-arch-sample/internal/foo/publisher"
+)
+
+func PublishNewFooUsecase(input CreateFooInbound, repo publisher.FooPublisher) (CreateFooOutbound, error) {
+	repo.Add(input.Entity())
+}
