8e588c13e5e838a825462b8ab53045cb00101570
Gaya <rrgaya@gmail.com>
2022-12-06 09:06:03 -0300

    Added mock in PostFoo and call to usecase at controllers #1

--- pkg/controllers/foo_controllers.go
+++ pkg/controllers/foo_controllers.go
@@ -1,12 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/qbem-repository/golang-arch-sample/internal/foo/entity"
+	"github.com/qbem-repository/golang-arch-sample/internal/foo/repository"
+	"github.com/qbem-repository/golang-arch-sample/internal/foo/usecase"
+)
 
 func GetFoo(w http.ResponseWriter, r *http.Request) {
+
 	w.Write([]byte("foo"))
 }
 
 func PostFoo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("foo"))
+	// MOCK
+	jsonMock := `
+	{
+	"Foo": "value"
+	}
+	`
+	var responseFoo entity.Foo
+	repoFoo := new(repository.FooRepository)
+	// END MOCK
+
+	json.NewDecoder(strings.NewReader(jsonMock)).Decode(&responseFoo)
+
+	usecase.NewSaveNewFooUsecase(*repoFoo)
+
 	w.WriteHeader(http.StatusCreated)
+
+	w.Write([]byte("{message: success}"))
 }
